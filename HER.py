5b6cdb1fb59bc57a3a4bdc080ead36b4fc2124ae
ANITS60106 <ramazanalmazbekov63@gmail.com>
2024-10-11 13:33:06 +0600

    gora

---
+++ HER.py
@@ -0,0 +1,12 @@
+# def NIGGANIGGAXER(n, x):
+#
+#     firstn = pow(n, x)
+#     secondn = pow(2, firstn, 1000)
+#     result = secondn
+#     return result
+#
+# n = int(input("n is :"))
+# x = int(input("x is: "))
+# print(f'The last two niggers: {NIGGANIGGAXER(n,x)}')
+
+
---
+++ HOrniye_tsepy.py
@@ -0,0 +1,33 @@
+def tsepytuponaiti(h, k):
+    count = 1
+    for i in range(len(h) - 1):
+        if abs(h[i] - h[i + 1]) > k:
+            count += 1
+    return count
+
+def k_tasamayaraznitsaprokotoruyugovorilRuslan(n, m, h):
+    low = 0
+    high = 1000000
+    out = -1
+
+    while low <= high:
+        mid = (low + high) // 2
+        с = tsepytuponaiti(h, mid)
+
+        if с == m:
+            result = mid
+            high = mid - 1
+        elif с < m:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return result
+
+n, m = input().split()
+n, m = int(n), int(m)
+h = [int(x) for x in input().split()]
+
+out = k_tasamayaraznitsaprokotoruyugovorilRuslan(n, m, h)
+
+print(out)
